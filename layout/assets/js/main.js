46e29095317383a782ff828f016832535207a4ed
Ravi Suhag <suhag.ravi@gmail.com>
2014-05-12 11:23:56 +0530

    adding tabular view

--- layout/assets/js/main.js
+++ layout/assets/js/main.js
@@ -8,8 +8,8 @@ var chart = c3.generate({
             ['Demand Registered', 30, 200, 100, 400, 150, 250,30, 200, 100, 400, 150, 250],
             ['Work Allotted', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
             ['Unmet Demand', 100, 300, 170, 400, 200, 300,100, 300, 170, 400, 200, 300],
-            ['Unemplyment Allowances : Due for PDs', 90, 200, 70, 300, 100, 200,80, 200, 150, 200, 100, 100],
-            ['Unemplyment Allowances : Amount Payable', 70, 100, 170, 200, 230, 140,60, 120, 100, 240, 170, 200]
+            ['Unemployment Allowances : Due for PDs', 90, 200, 70, 300, 100, 200,80, 200, 150, 200, 100, 100],
+            ['Unemployment Allowances : Amount Payable', 70, 100, 170, 200, 230, 140,60, 120, 100, 240, 170, 200]
         ],
     // type: 'spline'
   },
