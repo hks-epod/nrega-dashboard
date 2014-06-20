fb936107c1757fdd2734efdf4e648aa348012984
Ravi Suhag <suhag.ravi@gmail.com>
2014-06-20 11:43:00 +0530

    persondays cleanup

--- layout/assets/js/main.js
+++ layout/assets/js/main.js
@@ -6,7 +6,7 @@ var chart = c3.generate({
     x: 'x',
     columns: [
             ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            ['Demand Registered', 30, 200, 100, 400, 150, 250,30, 200, 100, 400, 150, 250],
+            ['Demand Registered', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
             ['Labour Budget', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
         ],
     // type: 'spline'
@@ -27,7 +27,7 @@ var work_alloted_chart = c3.generate({
     x: 'x',
     columns: [
             ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            ['Work Alloted', 30, 200, 100, 400, 150, 250,30, 200, 100, 400, 150, 250]
+            ['Work Alloted', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
         ],
     // type: 'spline'
   },
@@ -47,7 +47,7 @@ var unmet_Demand_chart = c3.generate({
     x: 'x',
     columns: [
             ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            ['Unmet Demand', 30, 200, 100, 400, 150, 250,30, 200, 100, 400, 150, 250]
+            ['Unmet Demand', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
         ],
     // type: 'spline'
   },
@@ -61,21 +61,61 @@ var unmet_Demand_chart = c3.generate({
   }
 });
 
-var Unemployment_allowances_chart= c3.generate({
+var Unemployment_allowances_chart = c3.generate({
   bindto: '#unmet_Demand_chart',
   data: {
     x: 'x',
     columns: [
             ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            ['Due for PDs', 30, 200, 100, 400, 150, 250,30, 200, 100, 400, 150, 250],
+            ['Unemployment Allowances - Due for PDs', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
             ['Amount payable', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
 
         ],
     type: 'bar',
     types: {
-            'Due for PDs': 'line',
-            'Amount payable': 'bar',
-        },
+      'Unemployment Allowances - Due for PDs': 'line',
+      'Amount payable': 'bar',
+    },
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%m'
+      }
+    }
+  }
+});
+
+// Physical Charts 
+var phy_chart = c3.generate({
+  bindto: '#phy_chart',
+  data: {
+    x:'x',
+    columns: [
+            ['x', '2012', '2013', '2014'],
+            ['Persondays Yearly', 30, 200, 100],
+        ],
+    type: 'bar'
+  },
+  bar: {
+    width: {
+      ratio: 0.5
+    }
+  }
+});
+
+
+
+var persondaysmonthly_chart = c3.generate({
+  bindto: '#persondaysmonthly_chart',
+  data: {
+    x: 'x',
+    columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Persondays Monthly', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+    // type: 'spline'
   },
   axis: {
     x: {
@@ -92,43 +132,27 @@ var Unemployment_allowances_chart= c3.generate({
 
 
 
-var phy_chart = c3.generate({
-    bindto: '#phy_chart',
-    data: {
-        columns: [
-            ['HHs provided employment', 30, 200, 100, 400, 150, 250],
-            ['Work completion rate', 130, 100, 140, 200, 150, 50],
-            ['Total GPs', 230, 320, 210, 130, 250, 150],
-            ['GPs with nil expenditure', 100, 60, 90, 250, 100, 20]
-        ],
-        type: 'bar'
-    },
-    bar: {
-        width: {
-            ratio: 0.5 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
-    }
-});
-
-
 var chart = c3.generate({
-    bindto: '#fin_chart',
-    data: {
+  bindto: '#fin_chart',
+  data: {
 
-        columns: [
+    columns: [
             ['Started in No. of blocks(wages)', 120],
             ['Started in No. of blocks(Material)', 110],
             ['Started in No. of blocks(Admin)', 110],
         ],
-        type : 'donut'
+    type: 'donut'
+  },
+  donut: {
+    title: "eFMS",
+    onclick: function(d, i) {
+      console.log(d, i);
     },
-    donut: {
-        title: "eFMS",
-        onclick: function (d, i) { console.log(d, i); },
-        onmouseover: function (d, i) { console.log(d, i); },
-        onmouseout: function (d, i) { console.log(d, i); }
+    onmouseover: function(d, i) {
+      console.log(d, i);
+    },
+    onmouseout: function(d, i) {
+      console.log(d, i);
     }
+  }
 });
-
