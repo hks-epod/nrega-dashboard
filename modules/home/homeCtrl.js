0ac9776a7c1840f15eb92bf107dda91af99d41b5
Ravi Suhag <suhag.ravi@gmail.com>
2014-06-26 14:32:15 +0530

    Adding Angular Home Service

--- modules/home/homeCtrl.js
+++ modules/home/homeCtrl.js
@@ -1,7 +1,15 @@
 var home = angular.module('home', []);
 
-home.controller('homeCtrl', ['$scope', '$rootScope',
-  function($scope, $rootScope) {
+home.controller('homeCtrl', ['$scope', '$rootScope','YearlyReport',
+  function($scope, $rootScope, YearlyReport) {
+    
     $scope.hello= 'hello';
+    $scope.yearlydata=YearlyReport.testfetch;
+
+
+
+
+
+
   }
 ]);
