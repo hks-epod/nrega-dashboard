25a70f4c79ce75ab669c837877fcb3c54a5789dd
Ravi Suhag <suhag.ravi@gmail.com>
2014-07-01 00:46:00 +0530

    finishing block district and state select logic

--- modules/reportdash/selectors/selectorDirective.js
+++ modules/reportdash/selectors/selectorDirective.js
@@ -1,3 +1,17 @@
+reportdash.directive('selectyear', function() {
+  return {
+    restrict: 'AE',
+    template: '<select class="selector" ui-select2 ng-model="selectDistrict" data-placeholder="Select Year"><option value=""></option><option ng-repeat="year in years" value="{{$index}}">{{year}}</option></select>',
+    replace: true,
+    controller: ['$rootScope', '$scope',
+      function($rootScope, $scope) {
+      }
+    ],
+    link: function(scope, el, attrs) {
+    }
+  };
+});
+
 reportdash.directive('selectstate', function() {
   return {
     restrict: 'AE',
@@ -14,17 +28,13 @@ reportdash.directive('selectstate', function() {
 reportdash.directive('selectdistrict', function() {
   return {
     restrict: 'AE',
-    template: '<select class="selector" ui-select2 ng-model="selectDistrict" data-placeholder="Select District"><option value=""></option><option ng-repeat="(key, value) in districts" value="{{key}}">{{value}}</option></select>',
+    template: '<select class="selector" ui-select2 ng-model="selectedDistrict" data-placeholder="Select District"><option value=""></option><option ng-repeat="(key, value) in districts" value="{{key}}">{{value}}</option></select>',
     replace: true,
     controller: ['$rootScope', '$scope',
       function($rootScope, $scope) {
-
-        // All functions here
-
       }
     ],
-    link: function(scope, el, attrs) {
-    }
+    link: function(scope, el, attrs) {}
   };
 });
 
@@ -32,24 +42,13 @@ reportdash.directive('selectdistrict', function() {
 reportdash.directive('selectblock', function() {
   return {
     restrict: 'AE',
-    template: '<input id="rd-block" class="" ui-select2="blockselector" ng-model="selectedBlock">',
+    template: '<select class="selector" ui-select2 ng-model="selectedBlock" data-placeholder="Select Block"><option value=""></option><option ng-repeat="(key, value) in blocks" value="{{key}}">{{value}}</option></select>',
     replace: true,
     controller: ['$rootScope', '$scope',
       function($rootScope, $scope) {
-
-        // All functions here
-
       }
     ],
     link: function(scope, el, attrs) {
-      el.on('change', function(e) {
-
-        if (e.added) {
-          Tags.primary.coll = e.added.cat;
-          Tags.primary.id = e.added._id;
-          Tags.primary.text = e.added.text;
-        }
-      });
     }
   };
 });
@@ -62,46 +61,12 @@ reportdash.directive('selectgp', function() {
     replace: true,
     controller: ['$rootScope', '$scope',
       function($rootScope, $scope) {
-
-        // All functions here
-
       }
     ],
     link: function(scope, el, attrs) {
-      el.on('change', function(e) {
-
-        if (e.added) {
-          Tags.primary.coll = e.added.cat;
-          Tags.primary.id = e.added._id;
-          Tags.primary.text = e.added.text;
-        }
-      });
     }
   };
 });
 
 
-reportdash.directive('selectyear', function() {
-  return {
-    restrict: 'AE',
-    template: '<select class="selector" ui-select2 ng-model="selectDistrict" data-placeholder="Select Year"><option value=""></option><option ng-repeat="year in years" value="{{$index}}">{{year}}</option></select>',
-    replace: true,
-    controller: ['$rootScope', '$scope',
-      function($rootScope, $scope) {
 
-        // All functions here
-
-      }
-    ],
-    link: function(scope, el, attrs) {
-      el.on('change', function(e) {
-
-        if (e.added) {
-          Tags.primary.coll = e.added.cat;
-          Tags.primary.id = e.added._id;
-          Tags.primary.text = e.added.text;
-        }
-      });
-    }
-  };
-});
