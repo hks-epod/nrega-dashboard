6ee05343fec09fc56a199c3208b3c06ad2563b43
Ravi Suhag <suhag.ravi@gmail.com>
2014-07-03 14:07:19 +0530

    integrating all charts

--- modules/reportdash/reportdashCtrl.js
+++ modules/reportdash/reportdashCtrl.js
@@ -6,7 +6,7 @@ reportdash.controller('reportdashCtrl', ['$scope', '$rootScope', 'YearlyReport',
     ///////////////////////
     //  Region Handelers //
     ///////////////////////
-    
+
     $scope.years = ['2012-13', '2013-14', '2014-15'];
     Regions.fetch().then(function(data) {
       $scope.regions = data;
@@ -65,8 +65,8 @@ reportdash.controller('reportdashCtrl', ['$scope', '$rootScope', 'YearlyReport',
     //////////////////////////
     // Vizulization Loading //
     //////////////////////////
-    $scope.vizconfig = {
-      bindto: '#chart',
+    $scope.demand_labourbudget_chart = {
+      bindto: '#demand_labourbudget_chart',
       data: {
         x: 'x',
         columns: [
@@ -85,8 +85,8 @@ reportdash.controller('reportdashCtrl', ['$scope', '$rootScope', 'YearlyReport',
       }
     };
 
-    $scope.vizconfig2 = {
-      bindto: '#chart2',
+    $scope.work_alloted_chart = {
+      bindto: '#work_alloted_chart',
       data: {
         x: 'x',
         columns: [
@@ -104,6 +104,328 @@ reportdash.controller('reportdashCtrl', ['$scope', '$rootScope', 'YearlyReport',
         }
       }
     };
+
+    $scope.unmet_demand_chart = {
+      bindto: '#unmet_demand_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Unmet Demand', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+        // type: 'spline'
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+    $scope.unemployment_allowances_chart = {
+      bindto: '#unemployment_allowances_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Unemployment Allowances - Due for PDs', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+            ['Amount payable', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
+
+        ],
+        type: 'bar',
+        types: {
+          'Unemployment Allowances - Due for PDs': 'line',
+          'Amount payable': 'bar',
+        },
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+    $scope.persondays_yearly_chart = {
+      bindto: '#persondays_yearly_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '2012', '2013', '2014'],
+            ['Persondays : LB Approved Yearly', 30, 200, 100],
+        ],
+        type: 'bar'
+      },
+      bar: {
+        width: {
+          ratio: 0.5
+        }
+      }
+    };
+
+    $scope.gps_by_expenditure_chart = {
+      bindto: '#gps_by_expenditure_chart',
+      data: {
+
+        columns: [
+            ['GPs with nil expenditure', 20],
+            ['GPs with no employment generation in last month)', 30],
+            ['GPs with no approved works', 10],
+            ['GPs with no ongoing works', 40],
+        ],
+        type: 'donut'
+      },
+      donut: {
+        title: "Total GPs",
+        onclick: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseover: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseout: function(d, i) {
+          console.log(d, i);
+        }
+      }
+    };
+
+    $scope.persondays_monthly_chart = {
+      bindto: '#persondays_monthly_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Persondays : LB Approved Monthly', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+        // type: 'spline'
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+    $scope.persondays_pd_caste_chart = {
+      bindto: '#persondays_pd_caste_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Persondays:PD Generated for SC', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+            ['ST', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
+            ['Women', 110, 300, 120, 300, 350, 250, 100, 240, 230, 300, 230, 310],
+
+        ],
+        type: 'bar',
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+    $scope.hh_providedemployment_chart = {
+      bindto: '#hh_providedemployment_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['HH provided employment', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+        // type: 'spline'
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+    $scope.workcompletion_chart = {
+      bindto: '#workcompletion_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Work Completion Rate', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+        // type: 'spline'
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+
+     $scope.hhscompleted100days_chart = {
+      bindto: '#hhscompleted100days_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['HHs completed 100 days', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+        ],
+        // type: 'spline'
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+     $scope.wage_chart = {
+      bindto: '#wage_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Total', 130, 340, 200, 500, 250, 350, 130, 340, 200, 500, 250, 350],
+            ['Wage %', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+
+        ],
+        type: 'bar',
+        types: {
+          'Total': 'bar',
+          'Wage %': 'line',
+        },
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+     $scope.delayedpayment_chart = {
+      bindto: '#delayedpayment_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Delayed payment :PDs Payable', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+            ['Amount payable', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+        ],
+        type: 'bar',
+        types: {
+          'Delayed payment :PDs Payable': 'bar',
+          'Amount payable': 'line',
+        },
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+     $scope.wageperPD_chart = {
+      bindto: '#wageperPD_chart',
+      data: {
+        x: 'x',
+        columns: [
+            ['x', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            ['Average wage per PD', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+            ['Cost per PD', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250],
+        ],
+
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m'
+          }
+        }
+      }
+    };
+
+
+     $scope.efms_chart = {
+      bindto: '#efms_chart',
+      data: {
+
+        columns: [
+            ['Started in No. of blocks(wages)', 50],
+            ['Started in No. of blocks(Material)', 30],
+            ['Started in No. of blocks(Admin)', 20],
+        ],
+        type: 'donut'
+      },
+      donut: {
+        title: "eFMS",
+        onclick: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseover: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseout: function(d, i) {
+          console.log(d, i);
+        }
+      }
+    };
+
+     $scope.DBT_chart = {
+      bindto: '#DBT_chart',
+      data: {
+
+        columns: [
+            ['Active workers A/Cs freezed', 70],
+            ['Aadhar seeding against total active worker', 30],
+        ],
+        type: 'donut'
+      },
+      donut: {
+        title: "DBT",
+        onclick: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseover: function(d, i) {
+          console.log(d, i);
+        },
+        onmouseout: function(d, i) {
+          console.log(d, i);
+        }
+      }
+    };
+
 
 
     //////////////////////////
