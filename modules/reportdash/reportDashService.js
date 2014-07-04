17e81133576a88ef9ff0fcae061999d380fd2855
Ravi Suhag <suhag.ravi@gmail.com>
2014-07-04 14:12:46 +0530

    Adding service

--- modules/reportdash/reportDashService.js
+++ modules/reportdash/reportDashService.js
@@ -14,6 +14,22 @@ reportdash.factory('YearlyReport', ['$http',
     return YearlyReport;
 }]);
 
+reportdash.factory('MonthlyReport', ['$http',
+  function($http) {
+    var YearlyReport = function(data) {
+      angular.extend(this, data);
+    };
+
+    YearlyReport.fetch = function(code, year) {
+      return $http.get('api/dashboard_report_monthly.aspx?'+params.code_type+'_code='  + params.code+'&' +'fin_year='+ year +'&type='+params.type)
+        .then(function(response) {
+          return response.data;
+        });
+    };
+
+    return YearlyReport;
+}]);
+
 
 reportdash.factory('GPRegions', ['$http',
   function($http) {
