81cef3e1af14a8e3f5ab0967d135d60c106536e6
Ravi Suhag <suhag.ravi@gmail.com>
2014-10-14 10:41:48 +0530

    year selection logic

--- modules/pdp/pdpCtrl.js
+++ modules/pdp/pdpCtrl.js
@@ -1,8 +1,20 @@
 var pdp = angular.module('Pdp', []);
 
-pdp.controller('pdpCtrl', ['$scope','$window','$location', function($scope, $window, $location) {
+pdp.controller('pdpCtrl', ['$scope', '$window', '$location', 'Regions', 'GPRegions',
+  function($scope, $window, $location, Regions, GPRegions) {
+
+
+    $scope.years = ['2014-2015', '2013-2014', '2012-2013'];
+    $scope.selectedYear = $scope.years[0];
+    $scope.$watch('selectedYear', function() {
+
+      Regions.fetch($scope.selectedYear).then(function(data) {
+        $scope.regions = data;
+
+      });
+    });
 
 
 
-
-}]);
+  }
+]);
